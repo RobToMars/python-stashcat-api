f57a3f51163eb6ed9cae67483ac756aae6264379
Robert Schroeder <robert.schroeder@fmc-ag.com>
2021-11-15 00:02:18 +0100

    Added type assertion for endpoint wrapper Added typings

--- messenger/endpoint.py
+++ messenger/endpoint.py
@@ -4,6 +4,9 @@
 Endpoint
 """
 
+# Built-in Python library
+from typing import Union, Callable
+
 
 class Endpoint:
 
@@ -22,11 +25,14 @@ class Endpoint:
             print(f"Added endpoint: {end_point}")
 
 
-def endpoint(func):
+def endpoint(func: Union[classmethod, staticmethod, Callable]) -> Callable:
     def wrapper(instance, *args, **kwargs):
         end_point = f"/{instance.name}/{func.__name__}"
         Endpoint.add_end_point(end_point)
         response = instance.messenger.post(end_point=end_point)
+
+        assert callable(func), f"{func.__name__} needs to be callable in order to be wrapped by endpoint"
+
         return func(instance, *args, response=response, **kwargs)
 
     return wrapper
