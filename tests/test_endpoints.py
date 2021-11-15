bc6df297e41d059b314574dfc5d4bf25614072e6
Robert Schroeder <robert.schroeder@fmc-ag.com>
2021-11-15 19:02:49 +0100

    * Added channels endpoints

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -69,3 +69,33 @@ def test_account_endpoint_availability():
     }
     _test_availability(account_endpoint_methods)
 
+
+def test_channels_endpoint_availability():
+    channels_endpoint_methods = {
+        messenger.channels.addModeratorStatus: None,
+        messenger.channels.availableInvites: None,
+        messenger.channels.changePermissions: None,
+        messenger.channels.confirmMembershipRequest: None,
+        messenger.channels.create: None,
+        messenger.channels.createInvite: None,
+        messenger.channels.declineMembershipRequest: None,
+        messenger.channels.delete: None,
+        messenger.channels.edit: None,
+        messenger.channels.editDescription: None,
+        messenger.channels.editPassword: None,
+        messenger.channels.encryptionUpgradeAvailable: None,
+        messenger.channels.files: None,
+        messenger.channels.info: None,
+        messenger.channels.join: None,
+        messenger.channels.members: None,
+        messenger.channels.quit: None,
+        messenger.channels.recommendations: None,
+        messenger.channels.removeModeratorStatus: None,
+        messenger.channels.removeUser: None,
+        messenger.channels.rename: None,
+        messenger.channels.setUserWritePermissions: None,
+        messenger.channels.subscripted: None,
+        messenger.channels.upgradeChannelEncryption: None,
+        messenger.channels.visible: None
+    }
+    _test_availability(channels_endpoint_methods)
